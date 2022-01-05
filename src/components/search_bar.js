6ab31e19390daa1a3c71b4300ccdb655d70af3be
Roy Schor <70181314+RoySchor@users.noreply.github.com>
2022-01-05 00:00:31 -0500

    Created an input bar and an event handler to get console.log said input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+import React, { Component } from 'react';
+
+// A class-based component as opposed to functional
+class SearchBar extends Component {
+    render() {
+        // To tap into a browser event you must write
+        // onNameOfEvent={reference to event handler}
+        return <input onChange={(event) => console.log(event.target.value)} />;
+    }
+    
+    
+}
+// Only exports the search bar component, needed since each file is siloed from one another
+// So they need to be connected explicitly somehow
+export default SearchBar; 
+    
+/** 
+ * This is a function-based component
+const SearchBar = () => {
+  return <input />;  // This in JavaScript is React.creatElement(); thus why we need to import React
+};
+ 
+
+    * Naming Conventions: on or handler then name of element watching for event then name of event
+    * Example: onInputChange() or handleInputChange
+    * Whenever input changes run this or create an arrow function like we did above:
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
  */
-
-
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// if its a file we write we need to write a file reference as opposed to what we did above^
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDT-pvzAYs1Z9QBMADdI_Qw48wdGLkJAUw';
+
 // Create a new component. This component should produce some HTML.
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
